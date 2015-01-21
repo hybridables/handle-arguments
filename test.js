c31a3f4475bd77110313b26d43fc6cc541a760cb
tunnckoCore <mameto_100@mail.bg>
2015-01-21 04:22:11 +0200

    100% coverage

--- test.js
+++ test.js
@@ -13,54 +13,71 @@ var handleArguments = require('./index');
 describe('handle-arguments', function() {
   describe('should throw error', function() {
     it('if Object is given - handleArguments({name: 123})', function(done) {
-      assert.throws(function fixture() {
-        var argz = handleArguments({name: 123})
-        return argz;
-      }, /expect only Arguments object/);
+      function fixture() {
+        var argz = handleArguments({name: 123});
+      }
+      assert.throws(fixture, /expect only Arguments object/);
       done();
     });
 
     it('if no params is given - handleArguments()', function(done) {
-      assert.throws(function fixture() {
-        var argz = handleArguments()
-        return argz;
-      }, /expect only Arguments object/);
+      function fixture() {
+        var argz = handleArguments();
+      }
+      assert.throws(fixture, /expect only Arguments object/);
       done();
     });
 
     it('if Number is given - handleArguments(123)', function(done) {
-      assert.throws(function fixture() {
-        var argz = handleArguments(123)
-        return argz;
-      }, /expect only Arguments object/);
+      function fixture() {
+        var argz = handleArguments(123);
+      }
+      assert.throws(fixture, /expect only Arguments object/);
       done();
     });
 
     it('if Array is given - handleArguments([1,2,3])', function(done) {
-      assert.throws(function fixture() {
-        var argz = handleArguments([1,2,3])
-        return argz;
-      }, /expect only Arguments object/);
+      function fixture() {
+        var argz = handleArguments([1,2,3]);
+      }
+      assert.throws(fixture, /expect only Arguments object/);
+      done();
+    });
+
+    it('if String is given - handleArguments(\'123\')', function(done) {
+      function fixture() {
+        var argz = handleArguments('123');
+      }
+      assert.throws(fixture, /expect only Arguments object/);
       done();
     });
   });
 
-  it('should work with real Arguments object- handleArguments(arguments)', function(done) {
-    function fixture() {
-      var argz = handleArguments(arguments)
-      return argz;
-    }
+  describe('should work with real Arguments object', function() {
+    it('when dont have `function` as last argument', function(done) {
+      function fixture() {
+        var argz = handleArguments(arguments)
+        return argz;
+      }
+      var actual = fixture(1, 2, 3);
 
-    var actual = fixture(1, 2, 3, function __cb() {});
+      assert.deepEqual(actual.args, [1,2,3]);
+      assert.deepEqual(actual.arguments, [1,2,3]);
+      done();
+    });
 
-    assert.deepEqual(actual.args, [1,2,3]);
-    assert.deepEqual(actual.arguments, [1,2,3]);
-    assert.strictEqual(actual.callback.name, '__cb');
-    assert.strictEqual(typeof actual.callback, 'function');
+    it('when have `function` (cb) as last argument', function(done) {
+      function fixture() {
+        var argz = handleArguments(arguments)
+        return argz;
+      }
+      var actual = fixture(1, 2, 3, function __cb() {});
 
-    actual = fixture(1, 2, 3);
-    assert.deepEqual(actual.args, [1,2,3]);
-    assert.deepEqual(actual.arguments, [1,2,3]);
-    done();
+      assert.deepEqual(actual.args, [1,2,3]);
+      assert.deepEqual(actual.arguments, [1,2,3]);
+      assert.strictEqual(actual.callback.name, '__cb');
+      assert.strictEqual(typeof actual.callback, 'function');
+      done();
+    });
   });
 });
