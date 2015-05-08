65bc9220360712a6266266528b03f61b94bf91c6
tunnckoCore <mameto_100@mail.bg>
2015-05-09 00:47:28 +0300

    apply feross/standard

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
- * handle-arguments <https://github.com/tunnckoCore/handle-arguments>
+ * handle-arguments <https://github.com/hybridables/handle-arguments>
  *
  * Copyright (c) 2015 Charlike Mike Reagent, contributors.
  * Released under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-var slice = require('array-slice');
-var isArguments = require('is-arguments');
+var slice = require('array-slice')
+var isArguments = require('is-arguments')
 
 /**
  * Commonly used handling of Arguments object
@@ -16,19 +16,19 @@ var isArguments = require('is-arguments');
  * **Example:**
  *
  * ```js
- * var handleArguments = require('handle-arguments');
+ * var handleArguments = require('handle-arguments')
  *
  * function fixture() {
- *   var argz = handleArguments(arguments);
- *   return argz;
+ *   var argz = handleArguments(arguments)
+ *   return argz
  * }
  *
- * console.log(fixture(1, 2, 3));
+ * console.log(fixture(1, 2, 3))
  * //=> {callback: [Function: defaultHandleArgumentsCallback],
  * // arguments: [1, 2, 3], args: [1, 2, 3]}
  * //=> [Function: defaultHandleArgumentsCallback] is empty function (noop)
  *
- * console.log(fixture(1, 2, function cb() {}));
+ * console.log(fixture(1, 2, function cb() {}))
  * //=> {callback: [Function: cb], arguments: [1, 2], args: [1, 2]}
  * ```
  *
@@ -37,18 +37,18 @@ var isArguments = require('is-arguments');
  * @return {Object} with properties `callback`, `cb` and `arguments` with `args` alias
  * @api public
  */
-module.exports = function handleArguments(argsObject) {
+module.exports = function handleArguments (argsObject) {
   if (!isArguments(argsObject)) {
-    throw new TypeError('handle-arguments: expect only Arguments object');
+    throw new TypeError('handle-arguments: expect only Arguments object')
   }
-  var callback = function defaultHandleArgumentsCallback() {};
-  var args = slice(argsObject);
-  var len = args.length;
-  var last = args[len - 1];
+  var callback = function defaultHandleArgumentsCallback () {}
+  var args = slice(argsObject)
+  var len = args.length
+  var last = args[len - 1]
 
   if (typeof last === 'function') {
-    callback = last;
-    args = slice(args, 0, -1);
+    callback = last
+    args = slice(args, 0, -1)
   }
 
   return {
@@ -56,5 +56,5 @@ module.exports = function handleArguments(argsObject) {
     cb: callback,
     arguments: args,
     args: args
-  };
-};
+  }
+}
--- test.js
+++ test.js
@@ -1,88 +1,81 @@
 /**
- * handle-arguments <https://github.com/tunnckoCore/handle-arguments>
+ * handle-arguments <https://github.com/hybridables/handle-arguments>
  *
  * Copyright (c) 2015 Charlike Mike Reagent, contributors.
  * Released under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-var test = require('assertit');
-var handleArguments = require('./index');
+var test = require('assertit')
+var handleArguments = require('./index')
 
 var describe = test.describe
 var assert = test.assert
 var it = test.it
 
-describe('handle-arguments', function() {
-  describe('should throw error', function() {
-    it('when Object is given - handleArguments({name: 123})', function(done) {
-      function fixture() {
-        var argz = handleArguments({name: 123});
+describe('handle-arguments', function () {
+  describe('should throw error', function () {
+    it('when Object is given - handleArguments({name: 123})', function (done) {
+      function fixture () {
+        handleArguments({name: 123})
       }
-      assert.throws(fixture, /expect only Arguments object/);
-      done();
-    });
-
-    it('when no params is given - handleArguments()', function(done) {
-      function fixture() {
-        var argz = handleArguments();
+      assert.throws(fixture, /expect only Arguments object/)
+      done()
+    })
+    it('when no params is given - handleArguments()', function (done) {
+      function fixture () {
+        handleArguments()
       }
-      assert.throws(fixture, /expect only Arguments object/);
-      done();
-    });
-
-    it('when Number is given - handleArguments(123)', function(done) {
-      function fixture() {
-        var argz = handleArguments(123);
+      assert.throws(fixture, /expect only Arguments object/)
+      done()
+    })
+    it('when Number is given - handleArguments(123)', function (done) {
+      function fixture () {
+        handleArguments(123)
       }
-      assert.throws(fixture, /expect only Arguments object/);
-      done();
-    });
-
-    it('when Array is given - handleArguments([1,2,3])', function(done) {
-      function fixture() {
-        var argz = handleArguments([1,2,3]);
+      assert.throws(fixture, /expect only Arguments object/)
+      done()
+    })
+    it('when Array is given - handleArguments([1,2,3])', function (done) {
+      function fixture () {
+        handleArguments([1, 2, 3])
       }
-      assert.throws(fixture, /expect only Arguments object/);
-      done();
-    });
-
-    it('when String is given - handleArguments(\'123\')', function(done) {
-      function fixture() {
-        var argz = handleArguments('123');
+      assert.throws(fixture, /expect only Arguments object/)
+      done()
+    })
+    it("when String is given - handleArguments('123')", function (done) {
+      function fixture () {
+        handleArguments('123')
       }
-      assert.throws(fixture, /expect only Arguments object/);
-      done();
-    });
-  });
+      assert.throws(fixture, /expect only Arguments object/)
+      done()
+    })
+  })
 
-  describe('should work with real Arguments object', function() {
-    it('when dont have `function` as last argument', function(done) {
-      function fixture() {
-        var argz = handleArguments(arguments)
-        return argz;
+  describe('should work with real Arguments object', function () {
+    it('when dont have `function` as last argument', function (done) {
+      function fixture () {
+        return handleArguments(arguments)
       }
-      var actual = fixture(1, 2, 3);
+      var actual = fixture(1, 2, 3)
 
-      assert.deepEqual(actual.args, [1,2,3]);
-      assert.deepEqual(actual.arguments, [1,2,3]);
-      assert.deepEqual(actual.callback.name, 'defaultHandleArgumentsCallback');
-      done();
-    });
-
-    it('when have `function` (cb) as last argument', function(done) {
-      function fixture() {
-        var argz = handleArguments(arguments)
-        return argz;
+      assert.deepEqual(actual.args, [1, 2, 3])
+      assert.deepEqual(actual.arguments, [1, 2, 3])
+      assert.deepEqual(actual.callback.name, 'defaultHandleArgumentsCallback')
+      done()
+    })
+    it('when have `function` (cb) as last argument', function (done) {
+      function fixture () {
+        return handleArguments(arguments)
       }
-      var actual = fixture(1, 2, 3, function __cb() {});
+      var actual = fixture(1, 2, 3, function __cb () {})
 
-      assert.deepEqual(actual.args, [1,2,3]);
-      assert.deepEqual(actual.arguments, [1,2,3]);
-      assert.strictEqual(actual.callback.name, '__cb');
-      assert.strictEqual(typeof actual.callback, 'function');
-      done();
-    });
-  });
-});
+      assert.deepEqual(actual.args, [1, 2, 3])
+      assert.deepEqual(actual.arguments, [1, 2, 3])
+      assert.strictEqual(actual.callback.name, '__cb')
+      assert.strictEqual(typeof actual.callback, 'function')
+      done()
+    })
+  })
+})
