10fc7a4960e3df2af78f84b02edff2fef7119e5a
tunnckoCore <mameto_100@mail.bg>
2016-03-18 20:03:07 +0200

    start tests

--- test.js
+++ test.js
@@ -10,67 +10,79 @@
 'use strict'
 
 var test = require('assertit')
-var handleArguments = require('./index')
+var handle = require('./index')
 
-test('handle-arguments', function () {
-  test('should throw error', function () {
-    // test('when Object is given - handleArguments({name: 123})', function (done) {
-    //   function fixture () {
-    //     handleArguments({name: 123})
-    //   }
-    //   test.throws(fixture, /expect only Arguments object/)
-    //   done()
-    // })
-    // test('when no params is given - handleArguments()', function (done) {
-    //   function fixture () {
-    //     handleArguments()
-    //   }
-    //   test.throws(fixture, /expect only Arguments object/)
-    //   done()
-    // })
-    // test('when Number is given - handleArguments(123)', function (done) {
-    //   function fixture () {
-    //     handleArguments(123)
-    //   }
-    //   test.throws(fixture, /expect only Arguments object/)
-    //   done()
-    // })
-    // test('when Array is given - handleArguments([1,2,3])', function (done) {
-    //   function fixture () {
-    //     handleArguments([1, 2, 3])
-    //   }
-    //   test.throws(fixture, /expect only Arguments object/)
-    //   done()
-    // })
-    // test('when String is given - handleArguments("123")', function (done) {
-    //   function fixture () {
-    //     handleArguments('123')
-    //   }
-    //   test.throws(fixture, /expect only Arguments object/)
-    //   done()
-    // })
-  })
-
-  test('should work with real Arguments object', function () {
-    test('when do not have `function` as last argument', function (done) {
-      function fixture () {
-        return handleArguments(arguments)
-      }
-      var actual = fixture(1, 2, 3)
-
-      test.deepEqual(actual.arguments, [1, 2, 3])
-      test.ok(!actual.callback)
-      done()
-    })
-    test('when have `function` (cb) as last argument', function (done) {
-      function fixture () {
-        return handleArguments(arguments)
-      }
-      var actual = fixture(1, 2, 3, function callback () {})
-      test.deepEqual(actual.arguments, [1, 2, 3])
-      test.strictEqual(actual.callback.name, 'callback')
-      test.strictEqual(typeof actual.callback, 'function')
-      done()
-    })
-  })
+test('should return object with `.arguments` and `.callback` properties', function (done) {
+  function fixture () {
+    return handle(arguments)
+  }
+  var actual = fixture(1, 2, 3)
+  test.strictEqual(typeof actual === 'object', true)
+  test.ok(!actual.callback)
+  test.strictEqual(actual.callback, false)
+  test.deepEqual(actual.arguments, [1, 2, 3])
+  done()
 })
+
+// test('handle-arguments', function () {
+//   test('should throw error', function () {
+//     // test('when Object is given - handleArguments({name: 123})', function (done) {
+//     //   function fixture () {
+//     //     handleArguments({name: 123})
+//     //   }
+//     //   test.throws(fixture, /expect only Arguments object/)
+//     //   done()
+//     // })
+//     // test('when no params is given - handleArguments()', function (done) {
+//     //   function fixture () {
+//     //     handleArguments()
+//     //   }
+//     //   test.throws(fixture, /expect only Arguments object/)
+//     //   done()
+//     // })
+//     // test('when Number is given - handleArguments(123)', function (done) {
+//     //   function fixture () {
+//     //     handleArguments(123)
+//     //   }
+//     //   test.throws(fixture, /expect only Arguments object/)
+//     //   done()
+//     // })
+//     // test('when Array is given - handleArguments([1,2,3])', function (done) {
+//     //   function fixture () {
+//     //     handleArguments([1, 2, 3])
+//     //   }
+//     //   test.throws(fixture, /expect only Arguments object/)
+//     //   done()
+//     // })
+//     // test('when String is given - handleArguments("123")', function (done) {
+//     //   function fixture () {
+//     //     handleArguments('123')
+//     //   }
+//     //   test.throws(fixture, /expect only Arguments object/)
+//     //   done()
+//     // })
+//   })
+
+//   test('should work with real Arguments object', function () {
+//     test('when do not have `function` as last argument', function (done) {
+//       function fixture () {
+//         return handleArguments(arguments)
+//       }
+//       var actual = fixture(1, 2, 3)
+
+//       test.deepEqual(actual.arguments, [1, 2, 3])
+//       test.ok(!actual.callback)
+//       done()
+//     })
+//     test('when have `function` (cb) as last argument', function (done) {
+//       function fixture () {
+//         return handleArguments(arguments)
+//       }
+//       var actual = fixture(1, 2, 3, function callback () {})
+//       test.deepEqual(actual.arguments, [1, 2, 3])
+//       test.strictEqual(actual.callback.name, 'callback')
+//       test.strictEqual(typeof actual.callback, 'function')
+//       done()
+//     })
+//   })
+// })
