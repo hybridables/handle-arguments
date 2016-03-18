646ba096e60e80cc2504d1cdda2b28a8cae0e999
tunnckoCore <mameto_100@mail.bg>
2016-03-18 20:56:35 +0200

    update tests

--- test.js
+++ test.js
@@ -115,65 +115,20 @@ test('should be able to pass two indicies to `sliced`', function (done) {
   done()
 })
 
-// test('handle-arguments', function () {
-//   test('should throw error', function () {
-//     // test('when Object is given - handleArguments({name: 123})', function (done) {
-//     //   function fixture () {
-//     //     handleArguments({name: 123})
-//     //   }
-//     //   test.throws(fixture, /expect only Arguments object/)
-//     //   done()
-//     // })
-//     // test('when no params is given - handleArguments()', function (done) {
-//     //   function fixture () {
-//     //     handleArguments()
-//     //   }
-//     //   test.throws(fixture, /expect only Arguments object/)
-//     //   done()
-//     // })
-//     // test('when Number is given - handleArguments(123)', function (done) {
-//     //   function fixture () {
-//     //     handleArguments(123)
-//     //   }
-//     //   test.throws(fixture, /expect only Arguments object/)
-//     //   done()
-//     // })
-//     // test('when Array is given - handleArguments([1,2,3])', function (done) {
-//     //   function fixture () {
-//     //     handleArguments([1, 2, 3])
-//     //   }
-//     //   test.throws(fixture, /expect only Arguments object/)
-//     //   done()
-//     // })
-//     // test('when String is given - handleArguments("123")', function (done) {
-//     //   function fixture () {
-//     //     handleArguments('123')
-//     //   }
-//     //   test.throws(fixture, /expect only Arguments object/)
-//     //   done()
-//     // })
-//   })
+test('should push thruthy value argument to `.arguments` when not arguments or array is passed', function (done) {
+  test.strictEqual(handle('foo').callback, false)
+  test.deepEqual(handle('foo').arguments, ['foo'])
+  test.deepEqual(handle(123).arguments, [123])
+  test.deepEqual(handle(true).arguments, [true])
+  done()
+})
 
-//   test('should work with real Arguments object', function () {
-//     test('when do not have `function` as last argument', function (done) {
-//       function fixture () {
-//         return handleArguments(arguments)
-//       }
-//       var actual = fixture(1, 2, 3)
-
-//       test.deepEqual(actual.arguments, [1, 2, 3])
-//       test.ok(!actual.callback)
-//       done()
-//     })
-//     test('when have `function` (cb) as last argument', function (done) {
-//       function fixture () {
-//         return handleArguments(arguments)
-//       }
-//       var actual = fixture(1, 2, 3, function callback () {})
-//       test.deepEqual(actual.arguments, [1, 2, 3])
-//       test.strictEqual(actual.callback.name, 'callback')
-//       test.strictEqual(typeof actual.callback, 'function')
-//       done()
-//     })
-//   })
-// })
+test('should return object with empty `.arguments` when falsey value and not arguments/array', function (done) {
+  test.deepEqual(handle(false).arguments, [])
+  test.deepEqual(handle(false).callback, false)
+  test.deepEqual(handle(null).arguments, [])
+  test.deepEqual(handle(null).callback, false)
+  test.deepEqual(handle(undefined).arguments, [])
+  test.deepEqual(handle(undefined).callback, false)
+  done()
+})
