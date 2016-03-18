094147c2fba3587c442e97e6dd9ebca578051a41
tunnckoCore <mameto_100@mail.bg>
2016-03-18 20:14:46 +0200

    update tests

--- test.js
+++ test.js
@@ -11,16 +11,71 @@
 
 var test = require('assertit')
 var handle = require('./index')
+var isArray = require('isarray')
+
+function fixture () {
+  return handle(arguments)
+}
 
 test('should return object with `.arguments` and `.callback` properties', function (done) {
-  function fixture () {
-    return handle(arguments)
-  }
   var actual = fixture(1, 2, 3)
+
   test.strictEqual(typeof actual === 'object', true)
   test.ok(!actual.callback)
   test.strictEqual(actual.callback, false)
   test.deepEqual(actual.arguments, [1, 2, 3])
+  done()
+})
+
+test('should `.callback` property be falsey value if last argument is not a function', function (done) {
+  var actual = fixture(1, 2, 3)
+
+  test.strictEqual(typeof actual.callback === 'boolean', true)
+  test.strictEqual(actual.callback, false)
+  done()
+})
+
+test('should `.callback` property be falsey value if last argument is function but not a callback', function (done) {
+  /* istanbul ignore next */
+  function noop () {}
+  var actual = fixture(1, 2, noop)
+
+  test.strictEqual(typeof actual.callback === 'boolean', true)
+  test.strictEqual(actual.callback, false)
+  test.strictEqual(isArray(actual.arguments), true)
+  test.deepEqual(actual.arguments, [1, 2, noop])
+  done()
+})
+
+test('should `.callback` property be the callback function if is callback', function (done) {
+  /* istanbul ignore next */
+  function callback () {}
+  var actual = fixture(1, 2, callback)
+  test.strictEqual(typeof actual.callback === 'function', true)
+  test.strictEqual(actual.callback, callback)
+  done()
+})
+
+test('should `.arguments` property be array with all arguments without last if callback', function (done) {
+  /* istanbul ignore next */
+  function callback () {}
+
+  var actual = fixture(1, 2, 3, callback)
+  test.strictEqual(isArray(actual.arguments), true)
+  test.deepEqual(actual.arguments, [1, 2, 3])
+  done()
+})
+
+test('should be able to pass array instead of Arguments object', function (done) {
+  function boom (a, b, foo) {
+    return handle([a, b, foo])
+  }
+  var actual = boom('a', 2, fixture)
+
+  test.strictEqual(typeof actual.callback === 'boolean', true)
+  test.strictEqual(actual.callback, false)
+  test.strictEqual(isArray(actual.arguments), true)
+  test.deepEqual(actual.arguments, ['a', 2, fixture])
   done()
 })
 
